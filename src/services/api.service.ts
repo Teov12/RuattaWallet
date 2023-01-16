fe8b08feeaf5fdaecd1fb971dfa2f6fa75b0bdc3
Teo Camusso <teocamusso29@gmail.com>
2023-01-16 09:15:39 -0300

    .evn

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-	baseURL: 'https://laboratorio3-f36a.restdb.io/rest',
+	baseURL: import.meta.env.VITE_API_BASE_URL,
 	headers: {'x-apikey': '60eb09146661365596af552f'}
 });
